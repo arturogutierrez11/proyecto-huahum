3051592bff0ae88c8800e56b71ad93b28f00ae1a
arturogutierrez00 <arturo.g@bidcom.com.ar>
2023-12-14 15:45:06 -0200

    .env

--- api/routes/book.router.js
+++ api/routes/book.router.js
@@ -1,12 +1,9 @@
 const express = require("express")
 const router = express.Router()
 
-const bookController = require('../controllers/book.controller')
+const productController = require('../controllers/product.controller')
 
-router.get("/", bookController.getAll);
-router.get("/:id", bookController.getById);
-router.post("/", bookController.create);
-router.put("/:id", bookController.updateById);
-router.delete("/:id", bookController.deleteById);
+router.get("/", productController.getAll);
+
 
 module.exports = router
