fa21b00d294319a767f7edb5cbade8ea72382c38
arturogutierrez00 <arturo.g@bidcom.com.ar>
2023-12-14 15:09:15 -0200

    api folders

--- api/routes/book.router.js
+++ api/routes/book.router.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 const bookController = require('../controllers/book.controller')
 
-router.get("/", bookController.getAll)
-router.get("/:id", bookController.getById)
-router.post("/", bookController.create)
-router.put("/:id", bookController.updateById)
-router.delete("/:id", bookController.deleteById)
+router.get("/", bookController.getAll);
+router.get("/:id", bookController.getById);
+router.post("/", bookController.create);
+router.put("/:id", bookController.updateById);
+router.delete("/:id", bookController.deleteById);
 
 module.exports = router
